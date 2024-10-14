c96b4726efdb7c0b2d012c4e2f89b0e4b2d0cd60
Dharmin Tandel <dharmin28052002@gmail.com>
2024-10-14 09:56:44 +0530

    File Updated

--- app/src/main/java/com/tops/kotlin/navigationapp/NavigationDrawerActivity.kt
+++ app/src/main/java/com/tops/kotlin/navigationapp/NavigationDrawerActivity.kt
@@ -3,7 +3,10 @@ package com.tops.kotlin.navigationapp
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.tops.kotlin.navigationapp.databinding.ActivityNavigationDrawerBinding
+import com.tops.kotlin.navigationapp.fragments.HomeFragment
+import com.tops.kotlin.navigationapp.fragments.SettingsFragment
 
 class NavigationDrawerActivity : AppCompatActivity() {
 
@@ -17,21 +20,37 @@ class NavigationDrawerActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // add toggle button to drawer
-        toggle = ActionBarDrawerToggle(
-            this,
-            binding.drawerLayout,
-            binding.appBarLayout.toolbar,
-            R.string.open,
-            R.string.close
+        /*toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.appBarLayout.toolbar, R.string.open, R.string.close
         )
 
         toggle.drawerArrowDrawable.color = resources.getColor(R.color.white)
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)*/
 
-        // show menu icon and back icon while drawer open
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.materialToolbar.setNavigationOnClickListener {
+            binding.drawerLayout.open()
+        }
+
+        loadFragment(HomeFragment())
 
         // set navigation item selected listener
+        binding.navigationView.setNavigationItemSelectedListener {
+            binding.drawerLayout.close()
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                }
+
+                R.id.settings -> {
+                    loadFragment(SettingsFragment())
+                }
+            }
+            return@setNavigationItemSelectedListener true
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit()
     }
 }
